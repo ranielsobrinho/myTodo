27b8c3bb14f3eb1bba4d4bdaf9b0849769e67be1
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-19 12:19:18 -0300

    Add Home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './index.css';
+
+import Toolbar from '../../components/Toolbar';
+
+class Home extends Component{
+    render() {
+        return (
+            <div>
+                <Toolbar />
+            </div>
+        )
+    }
+}
+
+export default Home;
