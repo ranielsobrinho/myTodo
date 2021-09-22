d2c7934ecef08586f5853b86985f2eb931b40f22
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-22 17:57:55 -0300

    Add todo page component

---
+++ src/pages/TodosPage/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TodoForm from '../../components/TodoForm'
+
+export default function index() {
+    return (
+        <div>
+            <TodoForm />
+        </div>
+    )
+}
