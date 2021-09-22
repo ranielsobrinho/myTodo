988064f3d2d5f0ee90493473805c540db44d74a2
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-22 17:56:06 -0300

    Add password field

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,25 +2,39 @@ import React, {useState} from 'react';
 import './index.css'
 
 function Form(props){
-    const [input, setInput] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleChange = e => {
-        setInput(e.target.value);
+    const handleEmail = e => {
+        setEmail(e.target.value);
+    }
+    
+    const handlePassword = e => {
+        setPassword(e.target.value);
     }
 
     const handleSubmit = e => {
         e.preventDefault();
-        setInput('');
+        email('');
+        password('');
     }
 
     return(        
             <form onSubmit={handleSubmit}>
                 <input type="text" 
-                placeholder="Your email here" 
-                value={input} 
+                placeholder="E-mail" 
+                value={email} 
                 name="email" 
-                className="emailInput"
-                onChange={handleChange}/>
+                className="input"
+                onChange={handleEmail}/>
+
+                <input type="password"
+                placeholder="Password"
+                value={password}
+                name="password"
+                className="input"
+                onChange={handlePassword}
+                />
 
                 <button className="submit">{props.name}</button>
             </form>
