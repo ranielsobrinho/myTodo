2bc643dc2881ec1bc54db6ee0a26e4d65c6f4b20
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-22 17:57:42 -0300

    Add todo form component

---
+++ src/components/TodoForm/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function TodoForm() {
+    return (
+        <div>
+            <form>
+                <input 
+                placeholder="Todo here"/>
+
+                <button>Add</button>
+            </form>
+        </div>
+    )
+}
