74c95170e78510e6e83b3413f74577108a23250b
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-27 23:15:04 -0300

    Sign in component

---
+++ src/components/SignInForm/index.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import api from '../../services/Api';
+
+export default function SignInForm (){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmail = e => {
+        setEmail(e.target.value);
+    }
+    
+    const handlePassword = e => {
+        setPassword(e.target.value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setEmail('');
+        setPassword('');
+
+        api.post('/token', {
+            email,
+            password
+        }).then((response) => {
+            console.log(response.data);
+        }).catch((err) => console.log(err));
+    }
+
+    return(        
+            <form onSubmit={handleSubmit}>
+                <input type="text" 
+                placeholder="E-mail" 
+                value={email} 
+                name="email" 
+                className="input"
+                onChange={handleEmail}/>
+
+                <input type="password"
+                placeholder="Password"
+                value={password}
+                name="password"
+                className="input"
+                onChange={handlePassword}
+                />
+
+                <button className="submit">SignIn</button>
+            </form>
+    );
+}
