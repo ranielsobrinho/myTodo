3defaa3ff3b6ffd7eff819c859bcf3075a53e86b
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-27 23:15:55 -0300

    Todo Form

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from '../../services/Api';
 
 export default function TodoForm() {
+    const [todo, setTodo] = useState('');
+
+    const handleTodo = e => {
+        setTodo(e.target.value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setTodo('');
+
+        // TODO salvar o id do usuário e passá-lo aqui
+        api.post('/users/6/todos', todo)
+        .then((response) => {
+            console.log(response.data);
+        }).catch((err) => console.log(err));
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input 
-                placeholder="Todo here"/>
+                placeholder="Todo here"
+                value={todo}
+                name='todo'
+                onChange={handleTodo}
+                />
 
                 <button>Add</button>
             </form>
