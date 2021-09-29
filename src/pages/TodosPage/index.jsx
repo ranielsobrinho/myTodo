8b3ba9eb3b09563177111e259d9ce4b13fff1414
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-28 23:44:03 -0300

    Add Todos rendering component

--- src/pages/TodosPage/index.jsx
+++ src/pages/TodosPage/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import TodoForm from '../../components/TodoForm'
+
+import TodoForm from '../../components/TodoForm';
+import Todos from '../../components/Todos';
 
 export default function index() {
     return (
         <div>
             <TodoForm />
+            <Todos />
         </div>
     )
 }
