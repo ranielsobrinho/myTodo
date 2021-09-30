b04599d3129c2b7cf0e0e1a0ab59bfe76b342a93
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-29 23:51:49 -0300

    Sending token to authorizate request and response from api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+let data = sessionStorage.getItem('token');
+
 const api = axios.create({
-    baseURL: "http://localhost:3333/"
+    baseURL: "http://localhost:3333/",
+    headers: {'Authorization': `Bearer ${data}`}
 });
 
 export default api;
