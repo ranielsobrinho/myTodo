b3566f5354dc79d0d07e48d3be29b0fab62b9e63
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-03 17:03:28 -0300

    Add methods and buttons to done and exclude todo

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -10,18 +10,37 @@ export default function Todos(){
         const userId = sessionStorage.getItem('userId');
         Api.get(`/users/${userId}/todos`)
             .then((res) => {
-                setTodos(res.data);
-            }).catch(err => console.log(err));
-    }, []);
+                setTodos(res.data.filter(todo => todo.done === false));
+            })
+            .catch(err => console.log(err));
+        }, [todos]);
+        
+    function deleteTodo(id) {
+        Api.delete(`/todos/${id}`);
+        setTodos(todos.filter(todo => todo.id !== id));
+    }
+
+    function doneTodo(id) {
+        Api.put(`/todos/${id}`, {
+            done: true
+        })
+            .then((res) => {
+                alert('deu certo')
+            })
+            .catch(err => alert(err));
+    }
 
     return(
         <div className="container">
             {todos
-                .map((todo, index) => {
+                .map((todo, id) => {
                     return (
-                        <div key={index} className="todos">
+                        <div key={id} className="todos">
                             <p>{todo.content}</p>
-                            
+                            <div className="buttons">
+                                <button onClick={ () => doneTodo(todo.id)} >V</button>
+                                <button onClick={ () => deleteTodo(todo.id) }>X</button>
+                            </div>
                         </div>
                     )
                 })
