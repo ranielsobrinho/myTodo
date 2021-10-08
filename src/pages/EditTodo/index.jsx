14e54242c9bff5d1c0f2827127c9d726f50fc026
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-07 21:05:13 -0300

    Edit paga styled

--- src/pages/EditTodo/index.jsx
+++ src/pages/EditTodo/index.jsx
@@ -1,14 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
 
 import './index.css'
-import TodoForm from '../../components/TodoForm'
+import {MdEdit} from 'react-icons/md';
+import Button from '../../components/Button';
+import Api from '../../services/Api'
 
 export default function EditTodo() {
+    const [edited, setEdited] = useState('');
+
+    const {id} = useParams();
+    const history = useHistory()
+
+    useEffect(() => {
+        Api.get(`/todos/${id}`)
+        .then((res) => {
+            setEdited(res.data.content)
+        })
+    }, [id])
+
+    const handleEdited = e => {
+        setEdited(e.target.value);
+    }
+    
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setEdited('');
+
+        await Api.put(`/todos/${id}`, {
+            content: edited
+        })
+        .then( () => {
+                history.push('/todos')
+            }
+        )
+        .catch(err => console.log(err))
+    }
+
+    function cancel() {
+        history.push('/todos');
+    }
 
     return (
         <div className="edit">
             <h3>Editar todo</h3>
-            <TodoForm />
+            <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="text"
+                placeholder="Add new todo"
+                value={edited}
+                name='todo'
+                onChange={handleEdited}
+                />
+
+                <button><MdEdit />Edit</button>
+                <Button name="Cancel" onClick={() => cancel()}/>
+            </form>
+        </div>
         </div>
     )
 }
