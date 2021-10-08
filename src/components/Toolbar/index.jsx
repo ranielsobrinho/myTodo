61a80bbe14485833530a145c47100dc6aa22a22c
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-07 21:03:02 -0300

    Using svg icon

--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './index.css'
 import SignInForm from '../SignInForm';
 
+import {BiTask} from 'react-icons/bi';
 export default function Toolbar () {
         return (
             <div className="Toolbar">
                 <nav>
-                    <h2 className="Logo">MyTodo</h2>
+                    <h2 className="Logo"><BiTask />MyTodo</h2>
                     <div className="LoginForm" >
                         <SignInForm />
                     </div>
