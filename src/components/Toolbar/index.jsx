8e94a09e5fce69947eeeefe346f6461f37dc9e6b
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-09 18:37:21 -0300

    Add logout button and method

--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import './index.css'
-import SignInForm from '../SignInForm';
 
-import {BiTask} from 'react-icons/bi';
+import { BiTask } from 'react-icons/bi';
+import { FiLogOut } from 'react-icons/fi';
 export default function Toolbar () {
-        return (
-            <div className="Toolbar">
-                <nav>
-                    <h2 className="Logo"><BiTask />MyTodo</h2>
-                    <div className="LoginForm" >
-                        <SignInForm />
-                    </div>
-                </nav>
-            </div>
-        )
+    const history = useHistory();
+
+    function logout(){
+        sessionStorage.removeItem('token');
+        sessionStorage.removeItem('userId');
+        history.push('/');
+    }
+
+
+    return (
+        <div className="Toolbar">
+            <nav>
+                <h2 className="Logo"><BiTask />MyTodo</h2>
+                <div>
+                    <button className="button" onClick={() => {logout()}}><FiLogOut />Logout</button>
+                </div>
+            </nav>
+        </div>
+    )
 }
