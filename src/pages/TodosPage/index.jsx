84d944e5413fb0e81526485c957920b6a4949613
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-09 18:38:12 -0300

    Render toolbar component in main page

--- src/pages/TodosPage/index.jsx
+++ src/pages/TodosPage/index.jsx
@@ -3,10 +3,12 @@ import './index.css';
 
 import TodoForm from '../../components/TodoForm';
 import Todos from '../../components/Todos';
+import Toolbar from '../../components/Toolbar'
 
 export default function index() {
     return (
         <div className="todoPage">
+            <Toolbar />
             <TodoForm />
             <Todos />
         </div>
