3d3846762ad3110154abb03adab096623ff8b32b
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-11 16:02:18 -0300

    Add sweetalert and minimun length to input fields

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import api from '../../services/Api';
 import './index.css'
 import Button from '../Button';
+import swal from 'sweetalert'
 
 function RegisterForm(props){
     const [email, setEmail] = useState('');
@@ -32,32 +33,35 @@ function RegisterForm(props){
             email,
             password
         }).then(() => {
-            alert('Cadastro feito com sucesso! Faça o login e aproveite.');
+            swal("Parabéns","Cadastro feito com sucesso! Faça o login e aproveite.", "success");
         }).catch((err) => alert('Houve um problema no envio dos dados.'));
     }
 
     return(        
             <form onSubmit={handleSubmit}>
                 <input type="text" 
-                placeholder="Name" 
+                placeholder="Nome" 
                 value={name} 
                 name="name" 
                 className="input"
-                onChange={handleName}/>
+                onChange={handleName}
+                minLength="3"/>
 
                 <input type="email" 
                 placeholder="E-mail" 
                 value={email} 
                 name="email" 
                 className="input"
-                onChange={handleEmail}/>
+                onChange={handleEmail}
+                minLength="8"/>
 
                 <input type="password"
-                placeholder="Password"
+                placeholder="Senha"
                 value={password}
                 name="password"
                 className="input"
                 onChange={handlePassword}
+                minLength="8"
                 />
 
                 <Button className="submit" name="Register"/>
