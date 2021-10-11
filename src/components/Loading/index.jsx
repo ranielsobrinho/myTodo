6e8eae02ae930224e674fa11a0db6d0355562637
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-11 15:57:21 -0300

    Add new loading component and styled it

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './index.css'
+
+export default function Loading() {
+    return (
+        <div className="loading">
+            <svg width="51px" height="50px" viewBox="0 0 51 50">
+                <rect y="0" width="13" height="50" fill="#5F9EA0">
+                    <animate attributeName="height" values="50;10;50" begin="0s" dur="1s" repeatCount="indefinite" />
+                    <animate attributeName="y" values="0;20;0" begin="0s" dur="1s" repeatCount="indefinite" />
+                </rect>
+                <rect x="19" y="0" width="13" height="50" fill="#5F9EA0">
+                    <animate attributeName="height" values="50;10;50" begin="0.2s" dur="1s" repeatCount="indefinite" />
+                    <animate attributeName="y" values="0;20;0" begin="0.2s" dur="1s" repeatCount="indefinite" />
+                </rect>
+                <rect x="38" y="0" width="13" height="50" fill="#5F9EA0">
+                    <animate attributeName="height" values="50;10;50" begin="0.4s" dur="1s" repeatCount="indefinite" />
+                    <animate attributeName="y" values="0;20;0" begin="0.4s" dur="1s" repeatCount="indefinite" />
+                </rect>
+            </svg>
+        </div>
+    )
+}
