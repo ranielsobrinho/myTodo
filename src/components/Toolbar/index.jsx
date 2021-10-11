61d367c1add40336f9b28d2f09787c9987e683c1
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-11 16:06:01 -0300

    Add button that links to user component page

--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 import './index.css'
 
-import { BiTask } from 'react-icons/bi';
+import { BiTask, BiUserCircle } from 'react-icons/bi';
 import { FiLogOut } from 'react-icons/fi';
 export default function Toolbar () {
     const history = useHistory();
@@ -13,12 +13,19 @@ export default function Toolbar () {
         history.push('/');
     }
 
+    function getUser() {
+        const id = sessionStorage.getItem('userId');
+
+        history.push(`/user/${id}`);
+    }
+
 
     return (
         <div className="Toolbar">
             <nav>
                 <h2 className="Logo"><BiTask />MyTodo</h2>
                 <div>
+                    <button className="button" onClick={() => {getUser()}}><BiUserCircle />You</button>
                     <button className="button" onClick={() => {logout()}}><FiLogOut />Logout</button>
                 </div>
             </nav>
