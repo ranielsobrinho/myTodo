3030044885f03db8f44f1ee5ee2b341f55974b9b
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-11 16:09:44 -0300

    Add user page

---
+++ src/pages/User/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserData from '../../components/UserData'
+import Toolbar from '../../components/Toolbar'
+
+export default function User() {
+    return (
+        <div>
+            <Toolbar />
+            <UserData />
+        </div>
+    )
+}
