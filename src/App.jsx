9182a68ca8cbcbfcbaad4828eed52e6759220106
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-10-11 15:55:31 -0300

    Add user route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import TodosPage from './pages/TodosPage'
 import EditTodo from './pages/EditTodo';
+import User from './pages/User';
 
 export default function App () {
     return (
@@ -12,6 +13,7 @@ export default function App () {
           <Route path="/" exact component={Home}/>
           <Route path="/todos" component={TodosPage}/>
           <Route path="/edit/:id" component={EditTodo}/>
+          <Route path="/user/:id" component={User}/>
         </Switch>
       </Router>
     )
